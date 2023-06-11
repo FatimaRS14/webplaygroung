f522033fbf951c5792614cb3e147aa159b0ed5d8
Fatty <faty.aome.rojas14@gmail.com>
2023-06-11 09:53:43 -0600

    Señales

--- registration/models.py
+++ registration/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 # Create your models here.
+#Se importara el usuario para entrar al profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    avatar = models.ImageField(upload_to='profiles', null=True, blank='True')
+    bio = models.TextField(null=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
+
+#Esto es para checar lo del regitro de cuenta y esta cuenta sin uso
+@receiver(post_save, sender=User)
+def ensure_profile_exist(sender, instance, **kwargs):
+    if kwargs.get("created", False):
+        Profile.object.get_or_create(user=instance)
+        print("Se acaba de crear un usuario y su perfil enlazado")
+
+#Señal que manda una función que ejecuta un código
